48b7cd43055b7563d9c9970364ba8375238df529
Sai Nihal <109018562+Nihalrt@users.noreply.github.com>
2023-09-22 11:20:31 -0700

    Update Client.java

--- Client.java
+++ Client.java
@@ -2,15 +2,20 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
+/**
+ * Client-side implementation of the Multi-Client Chat Application.
+ */
 public class ChatClient {
-    private static final String SERVER_HOST = "localhost";
-    private static final int SERVER_PORT = 12345;
+    private static final String SERVER_HOST = "localhost"; // The server's hostname (change if necessary)
+    private static final int SERVER_PORT = 12345; // The server's listening port
 
     public static void main(String[] args) {
         try (Socket socket = new Socket(SERVER_HOST, SERVER_PORT)) {
             System.out.println("Connected to Chat Server: " + socket);
 
             PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+
+            // Start a separate thread to handle server messages
             Thread serverThread = new Thread(new ServerHandler(socket));
             serverThread.start();
 
@@ -18,7 +23,7 @@ public class ChatClient {
             String message;
             while (true) {
                 message = scanner.nextLine();
-                writer.println(message);
+                writer.println(message); // Send the message to the server
                 writer.flush();
             }
         } catch (IOException e) {
@@ -26,6 +31,7 @@ public class ChatClient {
         }
     }
 
+    // This inner class handles messages received from the server
     private static class ServerHandler implements Runnable {
         private Socket socket;
 
@@ -37,7 +43,7 @@ public class ChatClient {
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
                 String message;
                 while ((message = reader.readLine()) != null) {
-                    System.out.println("Received message: " + message);
+                    System.out.println("Received message: " + message); // Print received messages
                 }
             } catch (IOException e) {
                 e.printStackTrace();
